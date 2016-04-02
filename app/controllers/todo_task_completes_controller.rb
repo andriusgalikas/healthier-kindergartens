fb36e2f91a22763e5b78ccd8549b80cd863a6338
Tom Dallimore <me@tomdallimore.com>
2016-04-02 21:41:05 +0700

    Continued work on parent/worker todo functionality.

--- app/controllers/todo_task_completes_controller.rb
+++ app/controllers/todo_task_completes_controller.rb
@@ -2,11 +2,16 @@ class TodoTaskCompletesController < ApplicationController
 
     def update
         set_todo_task_complete
+        set_todo
     end
 
     private
 
     def set_todo_task_complete
         @todo_task_complete = current_user.task_completes.find(params[:id])
+    end
+
+    def set_todo
+        @todo = @todo_task_complete.todo
     end
 end
--- app/jobs/todo_frequencies_job.rb
+++ app/jobs/todo_frequencies_job.rb
@@ -5,6 +5,7 @@ class TodoFrequenciesJob < ActiveJob::Base
         TodoComplete.active.each do |todo_complete|
             if todo_complete.created_at < todo_complete.todo.frequency_to_time
                 todo_complete.inactive!
+                todo_complete.task_completes.update_all(result: 2)
             end
         end
     end
