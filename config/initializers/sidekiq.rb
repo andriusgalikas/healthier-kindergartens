4a0af31d092c402a1758262b8ac22183becfd150
Tom Dallimore <me@tomdallimore.com>
2016-04-13 14:08:00 +0700

    Configured redis url.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  config.redis = { url: 'redis://h:pi24k520qhht4cbo5ogisnm5ek@ec2-46-137-82-111.eu-west-1.compute.amazonaws.com:23899' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  config.redis = { url: 'redis://h:pi24k520qhht4cbo5ogisnm5ek@ec2-46-137-82-111.eu-west-1.compute.amazonaws.com:23899' }
 end
