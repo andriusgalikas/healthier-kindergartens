75cf09cddba57c5d66b4b137185de7db276807e0
jean armeza <sjeanc911@gmail.com>
2016-05-30 23:38:42 +0800

    More refactor Add actual voting workflow

--- app/models/survey_answer.rb
+++ app/models/survey_answer.rb
@@ -1,14 +1,15 @@
 # == Schema Information
 #
-# Table name : survey_answers
+# Table name: survey_answers
 #
-#  id          :integer
+#  id          :integer          not null, primary key
 #  attempt_id  :integer
 #  question_id :integer
 #  option_id   :integer
 #  correct     :boolean
 #  created_at  :datetime
 #  updated_at  :datetime
+#
 
 class SurveyAnswer < ActiveRecord::Base
   belongs_to :survey_attempts, foreign_key: :attempt_id
--- app/models/survey_attempts.rb
+++ app/models/survey_attempts.rb
@@ -2,12 +2,13 @@
 #
 # Table name: survey_attempts
 #
+#  id               :integer          not null, primary key
 #  participant_id   :integer
 #  participant_type :string
 #  survey_id        :integer
 #  winner           :boolean
 #  score            :integer
-
+#
 
 class SurveyAttempts < ActiveRecord::Base
   belongs_to :surveys
