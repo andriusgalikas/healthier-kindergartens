52eabe93403e4fff4711d34c879b4a7eff15e1bd
jean armeza <sjeanc911@gmail.com>
2016-06-23 17:58:47 +0800

    Add Super Admin's upload template functionality

--- config/routes.rb
+++ config/routes.rb
@@ -117,12 +117,10 @@ Rails.application.routes.draw do
         resources :subjects, except: :show
         resources :message_templates do
           collection do
-            get  :choose_template
             get  :subject
             post :sub_subject
             post :recipient
             post :content
-            get  :upload_template
             get  :edit_filters
             post :filter
           end
