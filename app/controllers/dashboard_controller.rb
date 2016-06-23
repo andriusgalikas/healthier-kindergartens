012cc9a2d5173855de9f4abbc3c599e38442c4b0
salman aslam <salman@clustox.com>
2016-06-23 15:01:11 +0500

    added empty space

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,8 @@ class DashboardController < ApplicationController
     before_action :authenticate_subscribed!
 
     def index
-        render "dashboard/#{current_user.role}", format: [:html]
-    rescue ActionView::MissingTemplate
+      render "dashboard/#{current_user.role}", format: [:html]
+      rescue ActionView::MissingTemplate
         redirect_to current_user.admin? ? admin_root_url : root_url
     end
 end
