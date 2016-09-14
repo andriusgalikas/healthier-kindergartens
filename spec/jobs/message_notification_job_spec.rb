5f51ce1958d321a5e86965467491ebe546bcce95
jean armeza <sjeanc911@gmail.com>
2016-09-14 20:55:49 +0800

    Test specs - fix broken test specs on Todo

--- spec/jobs/message_notification_job_spec.rb
+++ spec/jobs/message_notification_job_spec.rb
@@ -16,63 +16,59 @@ describe MessageNotificationJob do
 
     context 'when message sender is a Daycare Manager ' do
       context 'and when message is for workers only, ' do
-        it 'should only send notifs to the workers under the daycare' do
-          dc_worker = create(:worker_user, department: @department_ABC)
-          create(:user_daycare, user: dc_worker, daycare: @daycare_ABC)
+        let(:dc_ABC_worker)    { create(:worker_user, department: @department_ABC) }
+        let(:dc_XYZ_worker)   { create(:worker_user, department: @department_XYZ) }
+        let(:message_template) { create(:message_template_for_workers) }
+        let(:message)          { create(:mngr_message_for_workers, message_template: message_template, owner: @manager_ABC) }
 
-          message_template = create(:message_template_for_workers)
-          message = create(:mngr_message_for_workers, message_template: message_template, owner: @manager_ABC)
+        it 'should only send notifs to the workers under the daycare' do
+          create(:user_daycare, user: dc_ABC_worker, daycare: @daycare_ABC)
 
           MessageNotificationJob.perform_now(message)
-          expect( dc_worker.notifications.size ).to eql(1)
+          expect( dc_ABC_worker.notifications.size ).to eql(1)
         end
 
         it 'should not send notifs to the workers outside the daycare' do
-          dc_worker = create(:worker_user, department: @department_XYZ)
-          create(:user_daycare, user: dc_worker, daycare: @daycare_XYZ)
-
-          message_template = create(:message_template_for_workers)
-          message = create(:mngr_message_for_workers, message_template: message_template, owner: @manager_ABC)
+          create(:user_daycare, user: dc_XYZ_worker, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
-          expect( dc_worker.notifications.size ).to eql(0)
+          expect( dc_XYZ_worker.notifications.size ).to eql(0)
         end
       end
 
       context 'and when message is for parents only, ' do
-        it 'should only send notifs to parents with children under the daycare' do
-          dc_parent = create(:parentee_user, department: @department_ABC)
-          create(:user_daycare, user: dc_parent, daycare: @daycare_ABC)
+        let(:dc_ABC_parent)    { create(:parentee_user, department: @department_ABC) }
+        let(:dc_XYZ_parent)    { create(:parentee_user, department: @department_XYZ) }
+        let(:message_template) { create(:message_template_for_parents) }
+        let(:message)          { create(:mngr_message_for_parents, message_template: message_template, owner: @manager_ABC) }
 
-          message_template = create(:message_template_for_parents)
-          message = create(:mngr_message_for_parents, message_template: message_template, owner: @manager_ABC)
+        it 'should only send notifs to parents with children under the daycare' do
+          create(:user_daycare, user: dc_ABC_parent, daycare: @daycare_ABC)
 
           MessageNotificationJob.perform_now(message)
-          expect( dc_parent.notifications.size ).to eql(1)
+          expect( dc_ABC_parent.notifications.size ).to eql(1)
         end
 
         it 'should not send notifs to other parents without children in the daycare' do
-          dc_parent = create(:parentee_user, department: @department_XYZ)
-          create(:user_daycare, user: dc_parent, daycare: @daycare_XYZ)
-
-          message_template = create(:message_template_for_parents)
-          message = create(:mngr_message_for_parents, message_template: message_template, owner: @manager_ABC)
+          create(:user_daycare, user: dc_XYZ_parent, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
-          expect( dc_parent.notifications.size ).to eql(0)
+          expect( dc_XYZ_parent.notifications.size ).to eql(0)
         end
       end
     end
 
     context 'when message sender is an Admin ' do
+
       context 'and when message is for workers only, ' do
+        let(:message)    { create(:admin_message_for_workers, owner: @admin) }
+        let(:worker_ABC) { create(:worker_user, department: @department_ABC) }
+        let(:worker_XYZ) { create(:worker_user, department: @department_XYZ) }
+        let(:parent_ABC) { create(:parentee_user, department: @department_ABC) }
+        let(:parent_XYZ) { create(:parentee_user, department: @department_XYZ) }
+
         it 'should send notifs to all users with worker role' do
-          message = create(:admin_message_for_workers, owner: @admin)
-
-          worker_ABC = create(:worker_user, department: @department_ABC)
           create(:user_daycare, user: worker_ABC, daycare: @daycare_ABC)
-
-          worker_XYZ = create(:worker_user, department: @department_XYZ)
           create(:user_daycare, user: worker_XYZ, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
@@ -82,12 +78,7 @@ describe MessageNotificationJob do
         end
 
         it 'should not send notifs to user who are not workers' do
-          message = create(:admin_message_for_workers, owner: @admin)
-
-          parent_ABC = create(:parentee_user, department: @department_ABC)
           create(:user_daycare, user: parent_ABC, daycare: @daycare_ABC)
-
-          parent_XYZ = create(:parentee_user, department: @department_XYZ)
           create(:user_daycare, user: parent_XYZ, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
@@ -98,13 +89,14 @@ describe MessageNotificationJob do
       end
 
       context 'and when message is for parents only, ' do
+        let(:message)    { create(:admin_message_for_parents, owner: @admin) }
+        let(:parent_ABC) { create(:parentee_user, department: @department_ABC) }
+        let(:parent_XYZ) { create(:parentee_user, department: @department_XYZ) }
+        let(:worker_ABC) { create(:worker_user, department: @department_ABC) }
+        let(:worker_XYZ) { create(:worker_user, department: @department_XYZ) }
+
         it 'should only send notifs to all users with parentee role' do
-          message = create(:admin_message_for_parents, owner: @admin)
-
-          parent_ABC = create(:parentee_user, department: @department_ABC)
           create(:user_daycare, user: parent_ABC, daycare: @daycare_ABC)
-
-          parent_XYZ = create(:parentee_user, department: @department_XYZ)
           create(:user_daycare, user: parent_XYZ, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
@@ -114,12 +106,7 @@ describe MessageNotificationJob do
         end
 
         it 'should not send notifs to user who are not parentees' do
-          message = create(:admin_message_for_parents, owner: @admin)
-
-          worker_ABC = create(:worker_user, department: @department_ABC)
           create(:user_daycare, user: worker_ABC, daycare: @daycare_ABC)
-
-          worker_XYZ = create(:worker_user, department: @department_XYZ)
           create(:user_daycare, user: worker_XYZ, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
@@ -130,9 +117,11 @@ describe MessageNotificationJob do
       end
 
       context 'and when message is for managers only, ' do
-        it 'should only send notifs to all users with manager role' do
-          message = create(:admin_message_for_managers, owner: @admin)
+        let(:message)    { create(:admin_message_for_managers, owner: @admin) }
+        let(:parent_ABC) { create(:parentee_user, department: @department_ABC) }
+        let(:parent_XYZ) { create(:parentee_user, department: @department_XYZ) }
 
+        it 'should only send notifs to all users with manager role' do
           MessageNotificationJob.perform_now(message)
 
           expect( @manager_ABC.notifications.size ).to eql(1)
@@ -140,12 +129,7 @@ describe MessageNotificationJob do
         end
 
         it 'should not send notifs to user who are not managers' do
-          message = create(:admin_message_for_managers, owner: @admin)
-
-          parent_ABC = create(:parentee_user, department: @department_ABC)
           create(:user_daycare, user: parent_ABC, daycare: @daycare_ABC)
-
-          parent_XYZ = create(:parentee_user, department: @department_XYZ)
           create(:user_daycare, user: parent_XYZ, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
@@ -156,13 +140,12 @@ describe MessageNotificationJob do
       end
 
       context 'when message is intended for more than 1 role, ' do
+        let(:message)    { create(:message, owner: @admin) }
+        let(:parent_ABC) { create(:parentee_user, department: @department_ABC) }
+        let(:worker_XYZ) { create(:worker_user, department: @department_XYZ) }
+
         it 'should send notifs to all uses with the specified role' do
-          message = create(:message, owner: @admin)
-
-          parent_ABC = create(:parentee_user, department: @department_ABC)
           create(:user_daycare, user: parent_ABC, daycare: @daycare_ABC)
-
-          worker_XYZ = create(:worker_user, department: @department_XYZ)
           create(:user_daycare, user: worker_XYZ, daycare: @daycare_XYZ)
 
           MessageNotificationJob.perform_now(message)
