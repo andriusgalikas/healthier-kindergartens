1dbbe72cd2bd588e567442ad2250dcc008b76c01
Erik <erik861112@hotmail.com>
2017-03-12 07:58:37 -0700

    Fixed support locale

--- config/initializers/supported_locales.rb
+++ config/initializers/supported_locales.rb
@@ -2,6 +2,8 @@ I18n.default_locale = :en
 
 SUPPORTED_LOCALES = {
   :en => 'English',
-  :no => 'Norwegian',
+  :nk => 'Norwegian',
   :fr => 'French'
 }
+
+I18n.available_locales = [:at, :au, :be, :bg, :ca, :cn, :hr, :cy, :cz, :dk, :ee, :fi, :fr, :de, :gr, :hu, :is, :ie, :it, :jp, :lv, :lt, :lu, :mt, :nl, :nz, :nk, :pl, :pt, :ro, :ru, :sa, :sk, :si, :kr, :es, :se, :tr, :ae, :gb, :us]
