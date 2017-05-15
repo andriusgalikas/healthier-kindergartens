c5ed095def56990a0e8d840f4adbf4279a26e9d2
Erik <erik861112@hotmail.com>
2017-05-14 23:56:53 -0700

    Fixed guideline js issues of Codeclime

--- app/assets/javascripts/healthChildcare.guideline.js
+++ app/assets/javascripts/healthChildcare.guideline.js
@@ -30,57 +30,63 @@ healthChildcare.guideline = {
     });    
   },
 */
-  managerMessageSentStep1: function(){
+  dashboarStep: function(){
     intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
       window.location.href = 'dash?multipage=true';
     });    
   },
 
-  managerMessageReceiveStep1: function(){
-    intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
-      window.location.href = 'dash?multipage=true';
-    });    
-  },
+  // managerMessageSentStep1: function(){
+  //   intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
+  //     window.location.href = 'dash?multipage=true';
+  //   });    
+  // },
 
-  managerIllnessStep1: function(){
-    introJs().setOption('doneLabel', 'Next Step').start().oncomplete(function() {
-      window.location.href = 'graph_step2?multipage=true';
-    });
-  },
+  // managerMessageReceiveStep1: function(){
+  //   intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
+  //     window.location.href = 'dash?multipage=true';
+  //   });    
+  // },
 
-  managerIllnessStep2: function(){
-    intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
-      window.location.href = 'graph_step1?multipage=true';
-    });    
-  },
+  // managerIllnessStep1: function(){
+  //   introJs().setOption('doneLabel', 'Next Step').start().oncomplete(function() {
+  //     window.location.href = 'graph_step2?multipage=true';
+  //   });
+  // },
 
-  managerTodoStep1: function(){
-    intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
-      window.location.href = 'dash?multipage=true';
-    });    
-  },
+  // managerIllnessStep2: function(){
+  //   intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
+  //     window.location.href = 'graph_step1?multipage=true';
+  //   });    
+  // },
 
-  managerTodoReportStep1: function(){
-    introJs().setOption('doneLabel', 'Next Step').start().oncomplete(function() {
-      window.location.href = 'report_step2?multipage=true';
-    });
-  },
+  // managerTodoStep1: function(){
+  //   intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
+  //     window.location.href = 'dash?multipage=true';
+  //   });    
+  // },
 
-  managerTodoReportStep2: function(){
-    introJs().setOption('doneLabel', 'Next Step').start().oncomplete(function() {
-      window.location.href = 'report_step3?multipage=true';
-    });
-  },
+  // managerTodoReportStep1: function(){
+  //   introJs().setOption('doneLabel', 'Next Step').start().oncomplete(function() {
+  //     window.location.href = 'report_step2?multipage=true';
+  //   });
+  // },
 
-  managerTodoReportStep3: function(){
-    intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
-      window.location.href = 'dash?multipage=true';
-    });    
-  },
+  // managerTodoReportStep2: function(){
+  //   introJs().setOption('doneLabel', 'Next Step').start().oncomplete(function() {
+  //     window.location.href = 'report_step3?multipage=true';
+  //   });
+  // },
 
-  managerSurveyStep1: function(){
-    introJs().start();
-  },
+  // managerTodoReportStep3: function(){
+  //   intro = introJs().setOption('doneLabel', 'Done').start().oncomplete(function() {
+  //     window.location.href = 'dash?multipage=true';
+  //   });    
+  // },
+
+  // managerSurveyStep1: function(){
+  //   introJs().start();
+  // },
 
   introGoToStep: function(nextPage, done='Next Step'){
     introJs().setOption('doneLabel', done).start().oncomplete(function() {
