e0633b8923673a300a223e484d528be506d24c12
Erik <erik861112@hotmail.com>
2017-06-22 08:21:44 -0700

    Fixed Sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,10 +8,10 @@ if Rails.env.development?
     end
 elsif Rails.env.production?
     Sidekiq.configure_server do |config|
-      config.redis = { url: 'redis://h:pfh29f5oi5ajqe41krigfniij09@ec2-79-125-14-22.eu-west-1.compute.amazonaws.com:13019' }
+      config.redis = { url: 'redis://h:pefa6510f42d060e3859cc33475262868237f8a0ef1ab04688e5bac6929f5eeae@ec2-34-252-123-40.eu-west-1.compute.amazonaws.com:28039' }
     end
 
     Sidekiq.configure_client do |config|
-      config.redis = { url: 'redis://h:pfh29f5oi5ajqe41krigfniij09@ec2-79-125-14-22.eu-west-1.compute.amazonaws.com:13019' }
+      config.redis = { url: 'redis://h:pefa6510f42d060e3859cc33475262868237f8a0ef1ab04688e5bac6929f5eeae@ec2-34-252-123-40.eu-west-1.compute.amazonaws.com:28039' }
     end
 end
