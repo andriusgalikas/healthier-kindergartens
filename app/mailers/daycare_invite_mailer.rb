636e159bc1a6ede81e976e724701cf203a52c485
talha876 <talhawasim.876@gmail.com>
2017-11-30 00:13:06 +0500

    email settings for strategic and govt partners

--- app/mailers/daycare_invite_mailer.rb
+++ app/mailers/daycare_invite_mailer.rb
@@ -4,13 +4,24 @@ class DaycareInviteMailer < ApplicationMailer
 
     def invite (email, subject, content, from_email, from_name)
 	    m = Mailin.new(ENV['SENDGRID_URL'], ENV['SENDGRID_API_KEY'])
-		data = { 
-			"personalizations" => [{"to" => [{"email" => email, "name" => "Daycare"}]}],
-			"from" => {"email" => from_email, "name" => from_name },
-			"subject" => subject,
-			"content" => [{"type" => "text/html", "value" => content}]
-		}
-	 
-		result = m.send_email(data)
+	    if email.is_a? Array
+			email.each do |email|
+				data = {
+					"personalizations" => [{"to" => [{"email" => email, "name" => "Daycare"}]}],
+					"from" => {"email" => from_email, "name" => from_name },
+					"subject" => subject,
+					"content" => [{"type" => "text/html", "value" => content}]
+				}
+				result = m.send_email(data)
+			end
+		else
+			data = {
+				"personalizations" => [{"to" => [{"email" => email, "name" => "Daycare"}]}],
+				"from" => {"email" => from_email, "name" => from_name },
+				"subject" => subject,
+				"content" => [{"type" => "text/html", "value" => content}]
+			}
+			result = m.send_email(data)
+		end
     end
 end
